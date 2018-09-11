4b7018af75cb3022b6cf72f4896bc084bae210a6
Thomas Charlat <thomas.charlat@gmail.com>
2018-09-11 12:11:53 +0200

    style(charlat): norme and misc updates

--- charlat/board.c
+++ charlat/board.c
@@ -3,9 +3,9 @@
 
 void initialize_board(t_cell board[81])
 {
-	int i;
+	int	i;
 
 	i = 0;
 	while (i < 81)
-		initialize_cell(&(board[i++]));
+		initialize_cell(board + i++);
 }
--- charlat/cell.h
+++ charlat/cell.h
@@ -1,13 +1,6 @@
 #ifndef CELL_H
 # define CELL_H
 
-typedef struct	s_cell
-{
-unsigned char	unavailable[9];
-unsigned char	degrees_of_freedom;
-unsigned char	value;
-unsigned char	has_value;
-} 				t_cell;
 /*
 ** degrees_of_freedom is 0 when all values are possible, 9 when no more values are possible.
 ** unavailable[x] == 1 means the value x is not available
@@ -16,9 +9,15 @@ unsigned char	has_value;
 ** sum of unavailable from 0 to 8 should equal degrees_of_freedom
 */
 
+typedef struct	s_cell
+{
+unsigned char	unavailable[9];
+unsigned char	degrees_of_freedom;
+unsigned char	value;
+unsigned char	has_value;
+}				t_cell;
+
 unsigned char	update_cell(t_cell *cell, unsigned char update);
 void			initialize_cell(t_cell *cell);
 
 #endif /* !CELL_H */
-
-
